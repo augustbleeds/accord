669c7e62e0fb42434433ee5766437e2ee811004d
Augustus Chang <augustus.chang@rutgers.edu>
2017-07-21 09:36:00 -0700

    clean up code a bit

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -2,33 +2,27 @@ const functions = require('firebase-functions');
 const app = require('express')();
 const admin = require('firebase-admin');
 var serviceAccount = require("./config.json");
-//const messaging = firebase.messaging();
 
 const quokkaURL = 'http://www.traveller.com.au/content/dam/images/g/u/n/q/h/0/image.related.articleLeadwide.620x349.gunpvd.png/1488330286332.png';
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: "https://accord-18bdf.firebaseio.com"
-// });
 
 admin.initializeApp(functions.config().firebase);
 
 const dbRootRef = admin.database().ref();
-//const database = fire.database();
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
 
-
-//User logging in
+/**
+ * Responds to a login request.
+ * 1. Parses the email (TODO: fix later to parse from '.edu' and not the first '.')
+ * 2. Sends back the value (null or truthy)
+ * 3. (front-end @ LoginScree.js conditions on the response, and handles appropriate route)
+ */
 app.post('/login', (req, res) => {
+  // TODO delete
   // admin.auth().signInWithEmailAndPassword(req.body.email, req.body.password).catch((error) => {
   //   res.json({error: error});
   // });
   //var pass = req.body.password
+  // TODO delete
+
   var email = req.body.email.split('.')[0];
   console.log(`Email user wants to login is ${JSON.stringify(email)}`);
   dbRootRef.child(`/User/${email}`).once('value')
@@ -37,14 +31,14 @@ app.post('/login', (req, res) => {
       res.json(userSnap.val())
     })
     .catch((err) => {res.json({error: err})})
-  // dbRootRef.child('/User/test@test').once('value')
-  //   .then((userSnap) => {res.json(userSnap.val())})
-  //   .catch((err) => {res.json({error: err})})
 });
 
-//User registering information
+/**
+ * Responds to a Registration Request
+ * 1. Construct new User Object to save
+ * 2. Save in firebase
+ */
 app.post('/register', (req, res) => {
-  // create a new User reference
   const newObj = {
     nickname: req.body.nickname,
     password: req.body.password,
@@ -56,78 +50,53 @@ app.post('/register', (req, res) => {
     friends: null,
     img: quokkaURL
   };
-  dbRootRef.child(`/User/${req.body.email.split('.')[0]}`).set(newObj)
+  var email = req.body.email.split('.')[0];
+  dbRootRef.child(`/User/${email}`).set(newObj)
     .then(() => {res.json(newObj)})
-    .catch((err) => res.send('ERROR'))
-  //const newUserRef = dbRootRef.child('User').push();
-  // create object for reference
-  console.log('HIT REGISTER')
-  // const newObj = {
-  //   nickname: 'aug',
-  //   gender: 'M',
-  //   email: 'aug@rutgers.edu',
-  //   school: 'Rutgers University',
-  //   searching: null,
-  //   desc: 'I am sad.',
-  //   friends: null,
-  //   img: 'http://www.traveller.com.au/content/dam/images/g/u/n/q/h/0/image.related.articleLeadwide.620x349.gunpvd.png/1488330286332.png',
-  //   _id: newUserRef.key,
-  // }
-
-  // newUserRef.set(newObj)
-  // .then(() => {res.json(newObj)})
-  // .catch((err) => res.send('ERROR'))
+    .catch((err) => res.json(null))
 });
 
+// TODO: after authentication
 app.get('/logout', (req, res) => {
   console.log('we did logout correctly');
   res.send('Hello3');
 });
 
-//Finding an array of objects friends of the current user
+/**
+ * Retrieving all friends belonging to a user
+ * sends back an array of objects / or empty array
+ */
 app.get('/user/friends/:id', (req, res) => {
   console.log('GETTING FRIENDS');
   var currId = req.params.id;
   dbRootRef.child(`/User/${currId}/friends`).once('value')
     .then((friendEmailsSnap) => {
-      var email_id = friendEmailsSnap.val();
-      if(!email_id){
+      // TODO: object of emails....figure out if email_ids have the .edu in them.
+      var emailsObj = friendEmailsSnap.val();
+      // send back empty array
+      if(!emailsObj){
         res.json([]);
         return;
       }
-      var emails = Object.keys(email_id);
-      // var objToReturn = {};
+      var emailsArr = Object.keys(emailsObj);
+      var FriendsPromise = emailsArr.map(email => (dbRootRef.child(`/User/${email}`).once('value')));
 
-      var FriendsPromise = emails.map(email => (dbRootRef.child(`/User/${email}`).once('value')));
       Promise.all(FriendsPromise)
         .then(friendsSnap => {
           var friendsObj = friendsSnap.map(friendSnap => (friendSnap.val()));
+          // send back array of objects
           res.json(friendsObj);
         })
-
-      // for (var i = 0; i < emails.length; i++) {
-      //   dbRootRef.child(`/User/${emails[i]}`).once('value')
-      //     .then((userSnap) => {
-      //       var userObj = userSnap.val();
-      //       objToReturn[emails[i]] = {
-      //         img: userObj.img,
-      //         nickname: userObj.nickname
-      //       };
-      //       // res.json(objToReturn);
-      //     })
-      // }
-      // // i got the friend's id (this is an object)
-      // //res.json(friendsSnap.val())
-      // res.json(objToReturn);
     })
-    // i need to get friend's imgs
+    .catch((err)-> {
+      console.log("error getting friends");
+      res.json(null);
+    })
 });
 
 //When 2 people add each other as friends
 app.post('/user/add', (req, res) => {
   console.log('getting 2 friends')
-  // var myId = '-Kp25oaf2W35ZAjU7ebZ';
-  // var friendId = '-Kp26_r2wh1z3JUytJyb';
   var myId = req.body.myId;
   var friendId = req.body.friendId;
   var updates = {};
@@ -141,16 +110,10 @@ app.post('/user/add', (req, res) => {
     .catch((err) => {
       res.json({error: err})
     })
-  // dbRootRef.child(`/User/${myId}/friends`).once('value')
-  //   .then((friendsSnap) => {
-  //     // i got the friend's id (this is an object)
-  //     res.json(friendsSnap.val())
-  //   })
 });
 
 //Finds profile of user
 app.get('/user/profile/:id', (req, res) => {
-  //var currId = '-Kp25oaf2W35ZAjU7ebZ';
   var currId = req.params.id;
   dbRootRef.child(`/User/${currId}`).once('value')
     .then((friendSnap) => {
@@ -184,7 +147,9 @@ app.post('/user/match/:category/:id', (req, res) => {
   }
 });
 
-
+/**
+ * Database listener
+ */
 exports.matchUsers = functions.database
   // listen for new users in ANY topic
   .ref('/Topic/{changedTopic}/{newUser}')
