7751849ed4d82ed29df6f98eee8c329675176bc9
Augustus Chang <augustus.chang@rutgers.edu>
2017-07-24 17:33:37 -0700

    add currentFriendReducer

--- frontend/frontend-accord/reducers/mainReducer.js
+++ frontend/frontend-accord/reducers/mainReducer.js
@@ -1,9 +1,10 @@
 import {combineReducers} from 'redux';
 import user from './userReducer';
 import navigator from './navigatorReducer';
+import currentFriend from './currentFriendReducer';
 
 const mainReducer = combineReducers({
-  user, navigator
+  user, navigator, currentFriend
 });
 
 export default mainReducer;
