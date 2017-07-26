f2c347085240d8c9a20543b1a56f17878debf5d7
Augustus Chang <augustus.chang@rutgers.edu>
2017-07-25 21:26:42 -0700

    some more house cleaning

--- frontend/frontend-accord/components/swiper.js
+++ frontend/frontend-accord/components/swiper.js
@@ -5,7 +5,9 @@ import Swiper from 'react-native-swiper';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 const SCREEN_HEIGHT = Dimensions.get('window').height;
-
+/*
+<TitleScreen
+ */
 export default class swiperStyles extends Component {
   render(){
     return (
@@ -15,29 +17,29 @@ export default class swiperStyles extends Component {
         dot={<View style={{backgroundColor: 'gray', width: 5, height: 5, borderRadius: 4, marginLeft: 3, marginRight: 3, marginTop: 3, marginBottom: 50}} />}
         activeDot={<View style={{backgroundColor: 'white', width: 5, height: 5, borderRadius: 4, marginLeft: 3, marginRight: 3, marginTop: 3, marginBottom: 50}} />}
         >
-        <View style={[styles.slide,{ backgroundColor: '#000000' }]}>
+        <View style={styles.slide}>
           <Image style={{width:260, bottom: 30, alignSelf: 'auto'}} source={require('../assets/icons/icon2.png')} />
-          <View level={10}><Text style={styles.Maintext}>Welcome</Text></View>
-          <View level={15}><Text style={styles.secondText}>Swipe to Learn More</Text></View>
+          <View><Text style={styles.Maintext}>Welcome</Text></View>
+          <View><Text style={styles.secondText}>Swipe to Learn More</Text></View>
         </View>
-        <View style={[styles.slide, { backgroundColor: '#000000' }]}>
+        <View style={styles.slide}>
           <Image resizeMode="cover" style={styles.imgStyle} source={require('../assets/icons/lock.png')} />
-          <View level={-10}><Text style={styles.Maintext}>Safe and Secure</Text></View>
-          <View level={5}><Text style={styles.secondText}>Accord is designed
+          <View><Text style={styles.Maintext}>Safe and Secure</Text></View>
+          <View><Text style={styles.secondText}>Accord is designed
             to help you connect with other students going through similar life events. Share in a safe and
             secure environment.
           </Text></View>
         </View>
-        <View style={[styles.slide, { backgroundColor: '#000000' }]}>
+        <View style={styles.slide}>
           <Image resizeMode="cover" style={styles.imgStyle2} source={require('../assets/icons/talk.png')} />
-          <View level={-10}><Text style={styles.Maintext}>Voice your Story</Text></View>
-          <View level={5}><Text style={styles.secondText}>
+          <View><Text style={styles.Maintext}>Voice your Story</Text></View>
+          <View><Text style={styles.secondText}>
             We believe that authenticity is cool, real, and emotionally healthy.
             It deserves a higher priority in our lives and should be built into the way we connect with others.
           </Text></View>
         </View>
-        <View style={[styles.slide,{ backgroundColor: '#000000' }]}>
-          <View level={8}><Text style={[styles.Maintext, {bottom: 15}]}>Start Now!</Text></View>
+        <View style={styles.slide}>
+          <View><Text style={[styles.Maintext, {bottom: 15}]}>Start Now!</Text></View>
         </View>
       </Swiper>
     </View>
@@ -56,57 +58,31 @@ const styles = {
     justifyContent: 'center',
     alignItems: 'center',
     width: SCREEN_WIDTH,
-    // showDoneButton: false,
-    // showSkipButton: false
+    backgroundColor: '#000000',
   },
-
   Maintext: {
     fontSize: 25,
-    // fontFamily: 'HelveticaNeue-BoldItalic',
     color: '#fff',
   },
-
   imgStyle: {
-    // display: 'block',
     justifyContent: 'flex-start',
     alignItems: 'center',
     bottom: 12,
-    // resizeMode: "cover",
     width: 220,
     height: 220,
   },
-
   imgStyle2: {
-    // display: 'block',fontFamily: 'HelveticaNeue-Bold',
     justifyContent: 'flex-start',
     alignItems: 'center',
     left: 10,
     bottom: 13,
-    // resizeMode: "cover",
     width: 230,
     height: 230,
   },
-
   secondText: {
     fontSize: 15,
-    // fontFamily: 'HelveticaNeue',
     color: '#808080',
     textAlign: 'center',
     padding: 20
-  },
-
-  buttonLogInStyle: {
-    flex: 1,
-    backgroundColor: '#000000',
-    // marginTop: 15,
-    // borderWidth: 2,
-    // borderRadius: 10,
-  },
-  buttonSignUpStyle: {
-    flex: 1,
-    backgroundColor: '#6adaa8',
-    // marginTop: 15,
-    // borderWidth: 2,
-    // borderRadius: 10,
   },
 };
