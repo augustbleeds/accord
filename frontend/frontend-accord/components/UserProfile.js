216a4f0bfa6446eed42b39ce3f01ec78f5da787b
Augustus Chang <augustus.chang@rutgers.edu>
2017-08-08 09:44:07 -0700

    update layout of profile page

--- frontend/frontend-accord/components/UserProfile.js
+++ frontend/frontend-accord/components/UserProfile.js
@@ -12,27 +12,49 @@ class UserProfile extends Component {
   render() {
     return (
       <View style={styles.profile}>
-        <Image style={{borderRadius: 75, width:150 , height: 150, top: 1, alignSelf: 'center'}} source={{uri: this.props.user.img}} />
-        <Text> <Text style={styles.mainText}>Signed in as: </Text> <Text style={styles.mainText2}> {this.props.user.email} </Text> </Text>
-        <Text> <Text style={styles.text}>Nickname: </Text> <Text style={styles.text2}> {this.props.user.nickname} </Text> </Text>
-        <Text> <Text style={styles.text}>School: </Text> <Text style={styles.text2}> {this.props.user.school} </Text> </Text>
-        <Text> <Text style={styles.text}>Gender: </Text> <Text style={styles.text2}> {this.props.user.gender} </Text> </Text>
-        <Text> <Text style={styles.text}>Description:</Text> <Text style={styles.text2}>{this.props.user.desc}</Text></Text>
+        <Image style={styles.img} source={{uri: this.props.user.img}} />
+        <View>
+          <Text style={styles.mainText}> Signed in as: </Text>
+          <Text style={styles.mainText2}> {this.props.user.email} </Text>
+        </View>
+        <View>
+          <Text style={styles.text}>Nickname: </Text>
+          <Text style={styles.text2}> {this.props.user.nickname} </Text>
+        </View>
+        <View>
+          <Text style={styles.text}>School: </Text>
+          <Text style={styles.text2}> {this.props.user.school} </Text>
+        </View>
+        <View>
+          <Text style={styles.text}>Gender: </Text>
+          <Text style={styles.text2}> {this.props.user.gender} </Text>
+        </View>
+        <View>
+          <Text style={styles.text}>Description:</Text>
+          <Text style={styles.text2}>{this.props.user.desc}</Text>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  img: {
+    borderRadius: 75,
+    width:150 ,
+    height: 150,
+    top: 1,
+    alignSelf: 'center'
+  },
   profile: {
     justifyContent: 'space-around',
-    alignItems: 'flex-end',
     width: SCREEN_WIDTH * 0.67,
+    flex: 1,
   },
   mainText:{
     fontWeight: 'bold',
-    // textAlign: 'left',
-    alignItems: 'flex-end',
+    textAlign: 'left',
+    alignItems: 'flex-start',
     paddingHorizontal: 10,
     fontSize: 15,
     color: '#3b9788',
@@ -47,7 +69,7 @@ const styles = StyleSheet.create({
   },
   mainText2:{
     fontWeight: 'bold',
-    // textAlign: 'right',
+    textAlign: 'right',
     alignItems: 'flex-end',
     paddingHorizontal: 10,
     fontSize: 15,
@@ -67,7 +89,7 @@ const styles = StyleSheet.create({
     alignItems: 'flex-end',
     fontWeight: 'bold',
     paddingHorizontal: 10,
-    // textAlign: 'left',
+    textAlign: 'left',
     ...Platform.select({
       ios: {
         fontFamily:'Avenir'
@@ -82,7 +104,7 @@ const styles = StyleSheet.create({
     fontSize: 15,
     fontWeight: 'bold',
     paddingHorizontal: 10,
-    // textAlign: 'right',
+    textAlign: 'right',
     alignItems: 'flex-end',
     ...Platform.select({
       ios: {
