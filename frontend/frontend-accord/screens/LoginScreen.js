fcd970e6356468da8b4a04dd20109c1be3ca84c3
Augustus Chang <augustus.chang@rutgers.edu>
2017-08-07 22:12:59 -0700

    put back height

--- frontend/frontend-accord/screens/LoginScreen.js
+++ frontend/frontend-accord/screens/LoginScreen.js
@@ -139,7 +139,7 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 	},
   input : {
-		height: 100,
+		height: 40,
 		backgroundColor: 'rgba(255,255,255,0.7)',
 		color: '#34495e',
 		paddingHorizontal: 10,
